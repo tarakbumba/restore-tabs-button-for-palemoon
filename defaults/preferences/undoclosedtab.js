ea87260ce1342ee63d79dca519f0281ac871389c
Atilla ÖNTAŞ <tarakbumba@gmail.com>
2021-04-11 01:57:12 +0300

    - Initial commit

---
+++ defaults/preferences/undoclosedtab.js
@@ -0,0 +1,10 @@
+pref("extensions.restoretabsbutton.version", "");
+pref("extensions.restoretabsbutton.removecontextentry", false);
+pref("extensions.restoretabsbutton.oldAppearance", false);
+pref("extensions.restoretabsbutton.firstRun", true);
+pref("extensions.restoretabsbutton.showOpenAll", true);
+pref("extensions.restoretabsbutton.clearHistory", true);
+pref("extensions.restoretabsbutton.modifier", "accel,shift");
+pref("extensions.restoretabsbutton.key", "z");
+pref("extensions.restoretabsbutton.openMenu", false);
+pref("extensions.restoretabsbutton.onTop", true);
